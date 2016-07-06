297abb59ef3f7eb83328a66d4e8294e22c67ad12
rodbate <rodbatejss@gmail.com>
2016-07-06 21:32:39 +0800

    add

---
+++ src/main/java/Test.java
@@ -0,0 +1,12 @@
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+
+/**
+ * Created by rodbate on 2016/7/6.
+ */
+public class Test {
+
+    public static void main(String[] args) {
+
+        //new LocalContainerEntityManagerFactoryBean().setJpaProperties();
+    }
+}
